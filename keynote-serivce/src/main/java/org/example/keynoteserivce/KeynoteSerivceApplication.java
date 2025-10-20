dade0302f31341254f8db451aae96511cba6d32a
mouad002 <elcosto02@gmail.com>
2025-10-20 17:16:03 +0100

    create all the services

---
+++ keynote-serivce/src/main/java/org/example/keynoteserivce/KeynoteSerivceApplication.java
@@ -0,0 +1,13 @@
+package org.example.keynoteserivce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeynoteSerivceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeynoteSerivceApplication.class, args);
+    }
+
+}
---
+++ keynote-serivce/src/test/java/org/example/keynoteserivce/KeynoteSerivceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.keynoteserivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeynoteSerivceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
